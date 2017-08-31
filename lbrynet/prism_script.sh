b38a2ebaefc6c7ca380df89817e06e0bc414bcbb
Kay Kurokawa <kay@lbry.io>
2017-08-31 19:51:52 -0400

    update prism reflector cluster

--- lbrynet/prism_script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# this makes sure the prism worker runs every second
-while :
-do
-    /app/bin/prism-worker; /app/bin/rq worker -b
-    sleep 1
-done
--- lbrynet/run.sh
+++ lbrynet/run.sh
@@ -5,12 +5,11 @@ if [ "$1" == "lbryum-only" ]; then
     tail -f /dev/null
 elif [ "$1" == "reflector-cluster" ]; then
     sleep 15
-    /app/bin/rq worker&
     redis-server&
     /app/bin/prism-server&
-    /src/prism_script.sh&
-    /app/bin/python /app/bin/lbrynet-daemon --verbose
-
+    /app/bin/prism-worker&
+    /app/bin/python /app/bin/lbrynet-daemon --verbose&
+    tail -f /dev/null
 else
     sleep 15
     /app/bin/python /app/bin/lbrynet-daemon --verbose
